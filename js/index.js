14ae5381a72d265d9ca52ec33371429d83a20194
fonely0903 <fonely0903@yahoo.com.tw>
2017-05-24 19:35:56 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(function(){
+$("li").click(function() {
+	if(this.id == '1')
+	{
+    $('html,body').animate({
+	        scrollTop: $(".Profile").offset().top},
+		'slow');
+	}
+	else if(this.id =='2')
+	{
+		$('html,body').animate({
+		  scrollTop: $(".Chatroom").offset().top},
+		      'slow');
+	}
+	else if(this.id =='3')
+	{
+		$('html,body').animate({
+	           scrollTop: $(".Search").offset().top},
+		               'slow');
+	}
+	});
+});
+$('.carousel').carousel();
